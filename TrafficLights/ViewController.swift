f7e4f05fc7ba9ccbcb00cb1dc9c7dfb613f61f62
Nikita Kelevra <nonamerz5@mail.ru>
2021-03-25 00:43:58 +0300

    It's Alive! TrafficLights is Work

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -18,10 +18,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        pushButtomLabel.layer.cornerRadius = 10
+        redLightLabel.layer.cornerRadius = 70
+        yellowLightLabel.layer.cornerRadius = 70
+        greenLightLabel.layer.cornerRadius = 70
+        
+        redLightLabel.alpha = 0.3
+        yellowLightLabel.alpha = 0.3
+        greenLightLabel.alpha = 0.3
+        
+        
     }
 
-    @IBAction func pushButtonPress() {
-    }
+    @IBAction func pushButtonPress(_ sender: UIButton) {
+        
+        if redLightLabel.alpha == 1.0 {
+            redLightLabel.alpha = 0.3
+            yellowLightLabel.alpha = 1.0
+
+            
+        } else if yellowLightLabel.alpha == 1.0 {
+            yellowLightLabel.alpha = 0.3
+            greenLightLabel.alpha = 1.0
+            
+        } else if greenLightLabel.alpha == 1.0 {
+            redLightLabel.alpha = 1.0
+            greenLightLabel.alpha = 0.3
+            
+        } else { redLightLabel.alpha = 1.0 }
+        }
+        
     
 }
 
